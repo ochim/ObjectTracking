ae9aac939760f7c861efeac6f4db77080c738a86
ochi <mune.ochi@gmail.com>
2019-02-11 20:37:10 +0900

    Initial Commit

---
+++ ObjectTracking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObjectTracking
+//
+//  Created by 越智宗洋 on 2019/02/11.
+//  Copyright © 2019年 越智宗洋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
